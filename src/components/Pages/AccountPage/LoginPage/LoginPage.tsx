14e7929cf0159c693aa4f5f25f9a24596c2148d8
gsStaa <gsstare@gmail.com>
2024-11-29 22:09:30 +0300

    Пустой коммит

--- src/components/Pages/AccountPage/LoginPage/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-import { Form } from '../FormUser/FormUser'
-import styles from './styles.module.css'
-import { useLogin } from '../../../../hooks/use-auth'
-
-export function LoginPage() {
-  const handleLogin = useLogin()
-  return <Form title='Войти' handleClick={handleLogin} />
-}
--- src/components/Pages/AccountPage/RegisterPage/RegisterPage.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './styles.module.css'
-import { Form } from '../FormUser/FormUser'
-import { useRegister } from '../../../../hooks/use-auth'
-
-export function RegisterPage() {
-  const handleRegister = useRegister()
-  return <Form title='Зарегестрироваться' handleClick={handleRegister} />
-}
